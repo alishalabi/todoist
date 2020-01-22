03bc7c0a58b4db4cd39d65f347f96bed340067ab
Ali Shalabi <ashalabi13@gmail.com>
2020-01-21 17:32:09 -0800

    Working on sidebar content. 25:48 of tutorial

---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from 'react-icons/fa';
+
+export const Sidebar = () =>
+ <div className="sidebar" data-testid="sidebar">
+  <ul className="sidebar__generic">
+    <li>Inbox</li>
+    <li>Today</li>
+    <li>Next 7 days</li>
+  </ul>
+ </div>
